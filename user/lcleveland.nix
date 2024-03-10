2a3957b3326fdfbcefd6a0dd83fcfaf21dce7335
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-03-10 16:06:42 -0500

    fix

--- user/lcleveland.nix
+++ user/lcleveland.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   users.users.lcleveland = {
     isNormalUser = true;
     description = "Lyle Cleveland";
     extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+    shell = pkgs.fish;
   };
 
   home-manager.users.lcleveland = {
--- user/lcleveland/config/fish.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs, ... }:
-{
-  users.users.lcleveland.shell = pkgs.fish;
-}
